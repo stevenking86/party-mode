9dd0ae0b046aa3093638d596eb676b57a72244b8
Lucas Nagle <lnagle@gmail.com>
2017-11-15 10:24:41 -0600

    Add volume-transitionTime mapping and handle variable number of lights

--- hue.js
+++ hue.js
@@ -1,4 +1,43 @@
-function adjustHue(lightType, dataPoint) {
+var hueAuth;
+
+hueAuth = "UP5z1ZV45PMbMEgtTmeSKrahFyQ8C1Jbs98L2ADp";
+
+function getNumLights(hueIp, numLights, callback) {
+    var headers, hueUrl, request, url;
+
+    if (numLights) {
+        callback(numLights);
+    } else {
+        requestNumLights(hueIp, function (lights) {
+            callback(lights);
+        });
+    }
+}
+
+function requestNumLights(hueIp, callback) {
+    hueUrl = "http://" + hueIp + "/api/" + hueAuth;
+    url = hueUrl + "/lights/";
+
+    headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    request = new Request(url, {
+        method: "GET",
+        headers: headers
+    });
+
+    fetch(request).then(function (res) {
+        res.json().then(function (data) {
+            callback(Object.keys(data).length);
+        });
+    }, function(error) {
+        if (error) {
+            console.log(error);
+        }
+    });
+}
+
+function calculateHue(lightType, dataPoint) {
     var hueMap, max, min;
 
     hueMap = {
@@ -22,29 +61,35 @@ function adjustHue(lightType, dataPoint) {
     return Math.round((dataPoint - 10) / (60 - 10) * (max - min) + min);
 }
 
-function adjustSaturation(volume) {
+function calculateSaturation(volume) {
     return Math.round(volume / 150 * (200 - 25) + 25);
+}
+
+function calculateTransitionTime(volume) {
+    if (volume >= 85) {
+        volume = 85;
+    }
+
+    return Math.round(25 - volume / 85 * 25);
 }
 
 // Y = (X-A)/(B-A) * (D-C) + C
 // Saturation Range: 25 (most saturated) to 200 (white)
 // Hue range: 0 to 65535
 
-function updateLight(lightType, lightNum, dataPoint, volume, transitiontime) {
-    var body, headers, hue, hueAuth, hueIp, hueUrl, request, sat, url
+function updateLight(lightType, lightNum, dataPoint, volume, hueIp) {
+    var body, headers, hue, hueUrl, request, sat, transitionTime, url;
 
-    hue = adjustHue(lightType, dataPoint);
-    sat = adjustSaturation(volume);
+    hue = calculateHue(lightType, dataPoint);
+    sat = calculateSaturation(volume);
+    transitionTime = calculateTransitionTime(volume);
 
-    hueIp = document.getElementById("hueIP").value;
-    hueAuth = "UP5z1ZV45PMbMEgtTmeSKrahFyQ8C1Jbs98L2ADp";
     hueUrl = "http://" + hueIp + "/api/" + hueAuth;
-
     url = hueUrl + "/lights/" + lightNum + "/state";
     body = {
         hue: hue,
         sat: sat,
-        transitiontime: transitiontime
+        transitiontime: transitionTime
     };
 
     headers = new Headers();
@@ -59,7 +104,7 @@ function updateLight(lightType, lightNum, dataPoint, volume, transitiontime) {
     fetch(request).then(function (res) {
         // console.log(res);
     }, function(error) {
-        // console.log(error);
+        console.log(error);
     });
 }
 
